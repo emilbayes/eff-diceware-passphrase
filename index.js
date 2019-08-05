d636b78819d057d263691d8c83e7992e3735d9cf
Emil Bay <github@tixz.dk>
2019-08-05 13:10:32 +0200

    Upgrade nanoassert

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var assert = require('assert')
+var assert = require('nanoassert')
 var wordlist = require('./wordlist.json')
 var sample = require('secure-sample')
 var shuffle = require('secure-shuffle')
@@ -10,8 +10,8 @@ var NUMBER_OF_TOKENS = wordlist.length
 var ENTROPY_PER_TOKEN = Math.log(NUMBER_OF_TOKENS) / Math.log(2)
 
 module.exports = function words (count) {
-  assert.ok(count > 0, 'count must be positive integer')
-  assert.ok(count < NUMBER_OF_TOKENS, 'count must be less than the number of tokens')
+  assert(count > 0, 'count must be positive integer')
+  assert(count < NUMBER_OF_TOKENS, 'count must be less than the number of tokens')
 
   var idx = sample(count, NUMBER_OF_TOKENS)
 
@@ -22,8 +22,8 @@ module.exports = function words (count) {
 
 module.exports.words = module.exports
 module.exports.entropy = function (minimum) {
-  assert.ok(minimum > 0, 'minimum entropy must be positive')
-  assert.ok(minimum <= NUMBER_OF_TOKENS * ENTROPY_PER_TOKEN, 'minimum entropy must be less than maximum possible entropy')
+  assert(minimum > 0, 'minimum entropy must be positive')
+  assert(minimum <= NUMBER_OF_TOKENS * ENTROPY_PER_TOKEN, 'minimum entropy must be less than maximum possible entropy')
 
   return module.exports(Math.ceil(minimum / ENTROPY_PER_TOKEN))
 }
